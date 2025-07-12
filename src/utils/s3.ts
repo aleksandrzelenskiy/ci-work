dbde276069c194ab68f15235cb301ae56f01998f
Aleksandr Zelenskiy <al.a.zelenskiy@gmail.com>
2025-07-12 21:52:32 +0800

    S3 utils keys name has been changed

--- src/utils/s3.ts
+++ src/utils/s3.ts
@@ -1,42 +1,43 @@
-// utils/s3.ts
 import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
 
 const BUCKET = process.env.AWS_S3_BUCKET!;
 const REGION = process.env.AWS_S3_REGION!;
+const ENDPOINT = process.env.AWS_S3_ENDPOINT!;
+const ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID!;
+const SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY!;
+
 console.log('REGION:', REGION);
 console.log('BUCKET:', BUCKET);
-
+console.log('ENDPOINT:', ENDPOINT);
 
 const s3 = new S3Client({
   region: REGION,
-  endpoint: process.env.S3_ENDPOINT,
+  endpoint: ENDPOINT,
+  forcePathStyle: true, // обязательно для REG.RU
   credentials: {
-    accessKeyId: process.env.S3_ACCESS_KEY_ID!,
-    secretAccessKey: process.env.S3_SECRET_ACCESS_KEY!,
+    accessKeyId: ACCESS_KEY_ID,
+    secretAccessKey: SECRET_ACCESS_KEY,
   },
 });
 
 export async function uploadBufferToS3(
-  fileBuffer: Buffer,
-  key: string,
-  contentType: string
+    fileBuffer: Buffer,
+    key: string,
+    contentType: string
 ) {
   try {
     const command = new PutObjectCommand({
       Bucket: BUCKET,
-      Key: key, // Имя файла/ключ в S3
-      Body: fileBuffer, // Содержимое файла (Buffer)
+      Key: key,
+      Body: fileBuffer,
       ContentType: contentType,
       ACL: 'public-read',
     });
+
     await s3.send(command);
 
-    // Формируем публичный URL
-    const fileUrl = `${process.env.S3_ENDPOINT!.replace(
-      /\/+$/,
-      ''
-    )}/${BUCKET}/${key}`;
-    return fileUrl;
+    return `${ENDPOINT.replace(/\/+$/, '')}/${BUCKET}/${key}`;
+
   } catch (error) {
     console.error('Ошибка при загрузке в S3:', error);
     throw error;
