27d3b63329306b110c33bf60269980af7388b046
Aleksandr Zelenskiy <al.a.zelenskiy@gmail.com>
2025-11-16 11:12:15 +0800

    app build error fix #5

--- src/app/utils/regions.ts
+++ src/app/utils/regions.ts
@@ -1,3 +1,4 @@
 export { REGIONS as RUSSIAN_REGIONS, REGION_MAP, REGION_ISO_MAP } from '@/app/constants/regions';
-export type RegionOption = (typeof RUSSIAN_REGIONS)[number];
+import type { RegionOption as RegionOptionConst } from '@/app/constants/regions';
+export type RegionOption = RegionOptionConst;
 export type RegionCode = RegionOption['code'];
